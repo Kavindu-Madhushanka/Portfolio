41a35b9d3811961244a13bc87720a9cc55a91373
Kavindu-Madhushanka <madu10275@gmail.com>
2026-03-27 14:37:27 +0530

    modify app,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import Navbar from "./assets/component/Navbar";
 const App = () => {
   return (
     <div className="bg-[#08142b] text-white scroll-smooth">
-      <p>Hello</p>
+      <Navbar />
     </div>
   );
 };
