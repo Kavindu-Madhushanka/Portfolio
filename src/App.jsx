1966a582e46682da49beb31671c2199af26d9fd4
Kavindu-Madhushanka <madu10275@gmail.com>
2026-03-27 21:34:57 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Navbar from "./assets/component/Navbar";
+import Hero from "./assets/component/Hero";
 const App = () => {
   return (
     <div className="bg-[#08142b] text-white scroll-smooth">
       <Navbar />
+      <Hero />
     </div>
   );
 };
