17476888f03f23b3e5cdde4d283ef45d47629660
Kavindu-Madhushanka <madu10275@gmail.com>
2026-03-28 00:30:03 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import About from "./assets/component/About";
 
 import Navbar from "./assets/component/Navbar";
 import Hero from "./assets/component/Hero";
+import Skills from "./assets/component/Skills";
 const App = () => {
   return (
     <div className="bg-[#08142b] text-white scroll-smooth">
       <Navbar />
       <Hero />
       <About />
+      <Skills />
     </div>
   );
 };
